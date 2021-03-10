4cb069e9586ad6c64504422e9234d9fb99c057b8
Jonathin <jonathinmakori@gmail.com>
2021-03-10 13:26:36 -0500

    this is our test script

---
+++ Demo1.java
@@ -0,0 +1,7 @@
+package testerscript;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("I got my first bike in the hood from crackhead Billy for actually 25$");
+    }
+}
